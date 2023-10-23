7079db722dfa3e056f26c16a648bce9ee5d838b9
Tomaz <tomaz.perez@outlook.com>
2023-10-23 22:37:24 +0200

    second first build

---
+++ src/main/java/com/maniac/config/LibraryEventsConsumerConfig.java
@@ -0,0 +1,9 @@
+package com.maniac.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+//@EnableKafka //only needed for older spring version
+public class LibraryEventsConsumerConfig {
+
+}
